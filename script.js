65a6190b8d739a05e3fdd387d556de4b0b7a28b5
jp-roisin <jeanpaul.roisin@protonmail.com>
2021-09-28 14:03:28 +0200

    premier push prepa

---
+++ script.js
@@ -0,0 +1,6 @@
+// Sélection des éléments
+let button = document.querySelector('#display');
+let comments = document.querySelector('#comments');
+
+// Cacher les commentaires
+comments.style.display = "none";
