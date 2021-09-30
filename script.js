9d79472ccea04886fd7b2e4178a54bdeb511fd43
Jean-Paul Roisin <72938245+jp-roisin@users.noreply.github.com>
2021-09-30 19:45:50 +0200

    FINITO

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let buttonShow = document.querySelector('#button-show');
 let buttonHide = document.querySelector('#button-hide');
 let hide = document.querySelector('#hide');
+let body = document.querySelector('body');
 
 // Cacher les éléments
 hide.style.display = "none";
@@ -12,10 +13,18 @@ buttonShow.addEventListener('click', ()=>{
     hide.style.display = "block";
     buttonShow.style.display = "none";
     buttonHide.style.display = "block";
-})
+});
 
 buttonHide.addEventListener('click', ()=>{
     hide.style.display = "none";
     buttonShow.style.display = "block";
     buttonHide.style.display = "none";
-})
+});
+
+
+// On affiche la scrollbar après 3s pour ne pas géner l'annimation sur les commentaires
+window.onload = ()=>{
+    setTimeout(()=>{
+        body.style.overflow = "visible";
+    }, 2000);
+};
